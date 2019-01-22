f0c1aab473c860f729e6207b0f552ea497f98cf9
JAVIER YECHOEN MURUZABAL ISHIGETANI <21752434@alumnos.uem.es>
2019-01-22 11:38:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a21752434/appfirebasecoches/javabeans/Coche.java
@@ -0,0 +1,42 @@
+package com.example.a21752434.appfirebasecoches.javabeans;
+
+public class Coche {
+
+    private String matricula;
+    private String modelo;
+    private String color;
+    private int anio;
+    private double precio;
+
+    public Coche() {
+    }
+
+    public Coche(String matricula, String modelo, String color, int anio, double precio) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.color = color;
+        this.anio = anio;
+        this.precio = precio;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+}
