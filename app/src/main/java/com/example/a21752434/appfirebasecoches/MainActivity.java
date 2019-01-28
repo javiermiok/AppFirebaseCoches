b16fbc3898d65f8050f0171947d73642464d3962
Javier <javiermiok@gmail.com>
2019-01-28 19:26:44 +0100

    Versión para borrar

--- app/src/main/java/com/example/a21752434/appfirebasecoches/MainActivity.java
+++ app/src/main/java/com/example/a21752434/appfirebasecoches/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a21752434.appfirebasecoches;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_coches, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.menu_insertar) {
+
+        } else if(item.getItemId() == R.id.menu_modificar) {
+
+        } else if(item.getItemId() == R.id.menu_consulta_todos) {
+
+        } else if(item.getItemId() == R.id.menu_consultar_anio) {
+
+        } else if(item.getItemId() == R.id.menu_finalizar) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
